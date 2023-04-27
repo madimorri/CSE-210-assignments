e17317cad7b90cf79b1fd4b4bf008f3c951e9d3b
madimorri <madisonmorrissey797@gmail.com>
2023-04-26 19:31:53 -0500

    Prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+        int guess = -1;
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic > guess)
+            {
+                Console.WriteLine("Guess Higher ");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Guess lower ");
+            }
+            else
+            {
+                Console.WriteLine("You guessed correct!");
+            }
+        }
     }
 }
